84922a5b63d04322eecb38a38d12b45d6b0c1932
Steve Mao <maochenyan@gmail.com>
2021-10-26 21:30:03 +1100

    init

---
+++ Readme.md
@@ -0,0 +1,7 @@
+> A very small utility helper in audio. Just like left-pad in audio
+
+## Install
+
+```
+yarn install process-audio
+```
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export const processAudio = function (a: Float32Array): Int16Array {
+  for (var b = a.length, d = new Int16Array(b); b--; )
+    d[b] = 32767 * Math.min(1, a[b]);
+
+  return d;
+};
